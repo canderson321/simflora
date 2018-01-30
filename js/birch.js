651811b0df6104d290e81f3b40870f74003d1c32
cameronanderson94 <cameronander94@gmail.com>
2018-01-29 16:39:42 -0800

    make growth more controlled

--- js/birch.js
+++ js/birch.js
@@ -1,45 +1,51 @@
-function BirchTree(level) {
+function BirchPart(level) {
 	
 	this.level = level;
+	console.log(level);
 	
-	this.childSections = [];
+	this.childParts = [];
 	this.timestamp = Date.now();
 	this.height = 0;
 	this.group = new THREE.Group();
 	this.mesh;
 	
 	this.branchTime = 2;
-	this.numBranches = 2;
+	this.numBranches = 4;
 	this.straight = true;
+	this.minBranchAngle = 25;
+	this.maxBranchAngle = 50;
 	
-	var birchBark;
+	var material;
 	var geometry;
 	
 
 	
 	if (this.level === 0) {
-		
-		birchBark = new THREE.MeshLambertMaterial( {color: 0xe8d8c1} );
+		material = new THREE.MeshLambertMaterial( {color: 0xe8d8c1} );
 		geometry = new THREE.CylinderGeometry(.07, .1, 1, 3, 1, true );
 		this.branchTime = 5;
+		this.numBranches = 5;
 		
-	} else if (this.level < 3) {
+	} else if (this.level < 5) {
 		
-		birchBark = new THREE.MeshLambertMaterial( {color: 0xe8d8c1} );
+		material = new THREE.MeshLambertMaterial( {color: 0xe8d8c1} );
 		geometry = new THREE.CylinderGeometry(.07, .1, 1, 3, 1, true );
+		this.numBranches = 3
+		this.minBranchAngle = 25;
+		this.maxBranchAngle = 65;
 		
-	} else if (this.level === 4) {
+	} else if (this.level === 6) {
 		
-		birchBark = new THREE.MeshLambertMaterial( {color: 0xe8d8c1} );
-		geometry = new THREE.BoxGeometry( 0.4, 1, 0.00001 );
-		this.numBranches = 2;
-		this.straight = false;
-		
-	} else{
-		
-		birchBark = new THREE.MeshLambertMaterial( {color: 0xe8d8c1} );
+		material = new THREE.MeshLambertMaterial( {color: 0xe8d8c1} );
 		geometry = new THREE.CylinderGeometry(.07, .1, 1, 3, 1, true );
-		this.branchTime = 1;
+		this.numBranches = 0;
+		this.straight = true;
+		
+	} else {
+		
+		material = new THREE.MeshStandardMaterial( {color: 0x00ff00} );
+		geometry = new THREE.BoxGeometry( 0.25, 0.5, 0.00001 );
+		geometry.translate(0, -0.25, 0);
 		this.numBranches = 0;
 		this.straight = false;
 		
@@ -47,5 +53,6 @@ function BirchTree(level) {
 	
 	
 	geometry.translate(0, .5, 0);
-	this.mesh = new THREE.Mesh(geometry, birchBark);
+	this.mesh = new THREE.Mesh(geometry, material);
+	this.group.add(this.mesh);
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,3 @@
-//Object prototype for chunk of tree
-function Section(group, mesh, branchTime) {
-	this.group = group;
-	this.mesh = mesh;
-	this.childSections = [];
-	this.timestamp = Date.now();
-	this.branchTime = branchTime;
-	this.length = 0;
-}
 
 //Get an Euler Rotation for a given x and y rotation
 function rotateAndTilt(x, y) {
@@ -14,71 +5,49 @@ function rotateAndTilt(x, y) {
 }
 
 //we'd make one of these constructors for every plant type
-function Birch() {
-	var birchBark = new THREE.MeshLambertMaterial( {color: 0xe8d8c1} );
-	var geometry = new THREE.CylinderGeometry(.07, .1, 1, 3, 1, true );
-	geometry.translate(0, .5, 0);
+function Tree(Type) {
+	
+	this.trunk = new Type(0);	
 
-	var trunkMesh = new THREE.Mesh(geometry, birchBark);
-	this.treeObject = new THREE.Group();
-	console.log(this.treeObject);
-	this.treeObject.add(trunkMesh);
-
-	this.trunk = new Section(this.treeObject, trunkMesh, 5);
-
-	this.branches = [];
-
-
+	
 	//we'd make one of these for every type of obj eg trunk, branch, twig, leaf
-	this.newBranch = function(parentSection) {
-		var birchBark = new THREE.MeshLambertMaterial( {color: 0xe8d8c1} );
-		var geometry = new THREE.CylinderGeometry(.07, .1, 1, 3, 1, true );
-		geometry.translate(0, .5, 0);
+	this.newBranch = function(parentPart) {
 
-		var branchMesh = new THREE.Mesh(geometry, birchBark);
-		var branchGroup = new THREE.Group();
-		branchGroup.add(branchMesh);
-
-		parentSection.group.add(branchGroup);
-		var branch = new Section(branchGroup, branchMesh, 2);
-		branch.length = 0;
-
-		parentSection.childSections.push(branch);
-		this.branches.push(branch);
+		var branch = new Type(parentPart.level + 1);
+		parentPart.group.add(branch.group);
+		parentPart.childParts.push(branch);
+		var minAngle = parentPart.minBranchAngle;
+		var maxAngle = parentPart.maxBranchAngle;
 
 		//manipulate position and rotation
-		var eu = rotateAndTilt(Math.random() * 25 + 25, Math.random() * 360);
-		branchGroup.quaternion.setFromEuler(eu);
-		branchGroup.position.y = parentSection.length;
-
+		branch.group.quaternion.setFromEuler(rotateAndTilt(Math.random() * (maxAngle-minAngle) + minAngle, Math.random() * 360));
+		branch.group.position.y = parentPart.height;
 
 	};
 	
 	var depth = 0;
-	var maxDepth = 6;
 //this should allow us to setup different rules for different mesh types, triggering on different conditions, but the time stuff is
-	this.updateSection = function(section) {
+	this.updateSection = function(part) {
 		depth++;
-		var age = (Date.now() - section.timestamp + 1)/1000;
+		var age = (Date.now() - part.timestamp + 1)/1000;
 		
-		section.length = Math.log(age / section.branchTime + 1) / depth;
+		part.height = Math.log(age / part.branchTime + 1) / depth;
 		
-		//section.length = age*.1;
-		section.mesh.scale.set(section.length, section.length, section.length);
+		part.mesh.scale.set(part.height, part.height, part.height);
 		
-		if (section.childSections.length === 0 && depth < maxDepth) {
-			this.newBranch(section);
+		if (part.straight) {
+			this.newBranch(part);
+			part.straight = false;;
 		};
 
 
-		if (age > section.branchTime && section.childSections.length === 1 && depth < maxDepth) {
-			this.newBranch(section);
-			this.newBranch(section);
+		while (age > part.branchTime && part.childParts.length < part.numBranches && depth < 7) {
+			this.newBranch(part);
 		};
 		
 		var self = this;
-		section.childSections.forEach(function(childSection) {
-			childSection.group.position.y = section.length;
+		part.childParts.forEach(function(childSection) {
+			childSection.group.position.y = part.height;
 			self.updateSection(childSection);
 		});
 		depth--;
@@ -97,8 +66,8 @@ $(document).ready(function() {
 	
 	//setup camera
 	var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-	camera.position.set(0, 2, 20);
-	camera.lookAt(new THREE.Vector3(0, 0, 0));
+	camera.position.set(0, 5, 17);
+	camera.lookAt(new THREE.Vector3(0, 5, 0));
 	
 	
 	//setup scene
@@ -114,14 +83,15 @@ $(document).ready(function() {
 	scene.add( light );
 
 	//make new birch object, add it's trunk mesh to scene
-	var birch = new Birch();
-	scene.add(birch.trunk.group);
+	var tree = new Tree(BirchPart);
+	scene.add(tree.trunk.group);
+	
 
 
 	//setup animation loop
 	function animate() {
-		birch.updateSection(birch.trunk);
-		birch.treeObject.rotation.y += 0.03;
+		tree.updateSection(tree.trunk);
+		tree.trunk.group.rotation.y += 0.005;
 		requestAnimationFrame(animate);
 		renderer.render(scene, camera);
 	};
